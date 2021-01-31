9cfea146f25905f4d723eb5aa8a64debadb09abe
Ekomobong Archibong <ekomboy012@gmail.com>
2021-01-31 18:21:11 +0100

    **

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Contact({imagesUrl}) {
+
+    const [fullname, setFullname] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [message, setMessage] = useState('');
+    
+    function changeFullname(e) {
+        setFullname(e.target.value);
+    }
+
+    function changeEmail(e) {   
+        setEmail(e.target.value);
+    }
+
+    function changePhone(e) {
+        setPhone(e.target.value);
+    }
+
+    function changeMessage(e) {
+        setMessage(e.target.value);
+    }
+
+    function submitMessage(e) {
+        e.preventDefault();
+    }
+
     return (
         <section className="contact" id="contact">
 
@@ -8,19 +34,18 @@ function Contact({imagesUrl}) {
         <h3 className="title">We'll get back to you via email in minutes</h3>
         
         <div className="row">
-        
             <div className="image">
                 <img src={`${imagesUrl}Email campaign-cuate.svg`} alt="" />
                 <p><small><a style = {{color:'black',fontSize:'4px'}} href="https://storyset.com/marketing">Illustration by Freepik Storyset</a></small></p>
             </div>
-        
+
             <div className="form-container">
                 <form action="">
-                    <input type="text" placeholder="Full name" />
-                    <input type="email" placeholder="Enter your email" />
-                    <input type="number" placeholder="Phone" />
-                    <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
-                    <input type="submit" value="message" />
+                    <input type="text" name = "fullname" value = {fullname} placeholder="Full name" onChange = {changeFullname} />
+                    <input type="email" name = "email" value = {email} placeholder="Enter your email" onChange = {changeEmail} />
+                    <input type="number" name = "phone" value = {phone} placeholder="Phone" onChange = {changePhone} />
+                    <textarea name = "message" id="" cols="30" rows="10" placeholder="Message" onChange = {changeMessage}>{message}</textarea>
+                    <input type="submit" value="message" onClick = {submitMessage} />
                 </form>
             </div>
         
